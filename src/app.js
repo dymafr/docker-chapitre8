b2b1d5fe839317af7f7d8b7e96472aaf36a33387
dymafr <contact@dyma.fr>
2023-02-17 19:06:34 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+
+const client = new MongoClient(
+  `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@db`
+);
+async function run() {
+  try {
+    await client.connect();
+    await client.db('admin').command({ ping: 1 });
+    console.log('CONNEXION DB OK !');
+    count = client.db('test').collection('count');
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+run().catch(console.dir);
+
+app.get('/err', (req, res) => {
+  process.exit(0);
+});
+
+app.get('/', (req, res) => {
+  count
+    .findOneAndUpdate(
+      {},
+      { $inc: { count: 1 } },
+      { returnNewDocument: true, upsert: true }
+    )
+    .then((doc) => {
+      const count = doc.value;
+      res.status(200).json(count ? count.count : 0);
+    });
+});
+
+app.listen(80);
